7fc5f546cda37f8fd423aa81787536f1f93d630d
Karthikeyan TS <keyashqa@gmail.com>
2025-05-12 23:14:56 +0530

    Fixed submodule issue, added frontend properly

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/errorhandler');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
+
+// Enable CORS for frontend
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+
+
+// Middleware to parse JSON and form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/goals', require('./routes/goalsCRUD'));
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve static files from the React frontend app
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'frontend', 'build', 'index.html'));
+  });
+}
+
+
+
+// ❗ Error handler should be after all routes
+app.use(errorHandler);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
